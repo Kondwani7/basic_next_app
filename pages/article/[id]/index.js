d542da344e72f3536d3604820af9b23c7d829910
Kondwani Ngulube <kondwanigideon.ngulube@gmail.com>
2022-01-12 13:30:12 +0200

    routing

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,5 +1,45 @@
-const article = () => {
-    return <div>This article</div>
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+
+const article = ({article}) => {
+    return <>
+        <h1>{article.title}</h1>
+        <p>{article.body}</p>
+        <br/>
+        <button>
+        <Link href='/'>Go Back</Link>
+        </button>
+        
+    </>
+}
+
+export const getStaticProps = async (context) => {
+    const res = await fetch (`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+    const article = await res.json()
+
+    return {
+        props: {
+            article
+        }
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch (`https://jsonplaceholder.typicode.com/posts`)
+
+    const articles = await res.json()
+
+    const ids = articles.map(article => article.id)
+
+    const paths = ids.map(id=> ({params: {id: id.toString()}}))
+
+    return {
+        paths,
+        fallback:false
+    }
+
 }
 
 export default article;
